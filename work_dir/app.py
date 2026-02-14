593b0abe78080cdb8f4538558588de46494f5fdc
dugongyete-ui <dugongyete@gmail.com>
2026-02-14 11:55:06 +0000

    Ok

--- work_dir/app.py
+++ work_dir/app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
-# Tambahkan route dan fungsi lainnya jika diperlukan
+@app.route('/projects')
+def projects():
+    projects = [
+        {'title': 'Proyek 1', 'description': 'Deskripsi proyek 1'},
+        {'title': 'Proyek 2', 'description': 'Deskripsi proyek 2'}
+    ]
+    return render_template('projects.html', projects=projects)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
